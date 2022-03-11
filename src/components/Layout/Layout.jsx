1cdb355166edb85494818d418f9e75af54791f22
Aleksei Ivanov <profpopoff@gmail.com>
2022-03-11 22:44:23 +0300

    very last cummit

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Outlet, useLocation  } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from '../Header/Header'
 import './Layout.css'
 
 export default function Layout() {
 
    // * Background and layout classes
-   const path = useLocation().pathname.split("/")[1].toLowerCase()
+   const path = useLocation().pathname.split(`${process.env.PUBLIC_URL}/`)[1]
    const location = path === '' ? 'home' : path
 
    // * Redndering Layout
